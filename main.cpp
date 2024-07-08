199237b3358113632d243f5d16e35f11ee2e6f4e
Alice (UsoltsevaSB) <madame.rogers@icloud.com>
2024-07-08 15:22:19 +0300

    max

--- main.cpp
+++ main.cpp
@@ -7,5 +7,10 @@ int main()
     cout << "Enter A and B: ";
     int a, b;
     cin >> a >> b;
-    cout << "A + B = " << a + b << '\n'  << "A - B = " << a - b << '\n';
+    cout << "A + B = " << a + b << '\n'
+         << "A - B = " << a - b << '\n'
+         << "A * B = " << a * b << '\n'
+         << "A / B = " << a / b << '\n'
+         << "max(A, B)" << max(a , b) <<'\n';
+
 }
